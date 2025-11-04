02d40bfb1588ebe07ef8c1b6e041a9845d808603
vondarkness <aroli.marcellinus@gmail.com>
2025-11-04 13:40:45 +0900

    first commit

---
+++ modules/neuron_bench.hpp
@@ -0,0 +1,11 @@
+#ifndef NEURON_BENCH_HPP
+#define NEURON_BENCH_CPP
+
+#include <types/parameter.hpp>
+
+#include <cstdio>
+
+int neuron_bench(const Parameter *p_param);
+void end_of_cycle_funct(short *pace_count);
+
+#endif
